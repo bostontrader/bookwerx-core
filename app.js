2138ca2fe4a830c82fe5a89546ecd977438d6178
Thomas Radloff <bostontrader@gmail.com>
2016-06-14 06:40:02 +0800

    Initial testing.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var express = require("express");
 var http = require("http");
-var app = express();
+var logger = require("morgan");
 
-app.use(function(request, response) {
-    console.log("In comes a request to: " + request.url);
-    response.end("Hello, world!");
+var app = express();
+module.exports = app;
+
+app.use(logger("short")); // morgan logger
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", function(req, res) {
+    res.type("text");
+    res.send(req.headers["user-agent"]);
 });
-http.createServer(app).listen(3000);
+
+
+app.listen(app.get("port"), function() {
+    console.log("App started on port " + app.get("port"));
+});
