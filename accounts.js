0ce358b4a8d736ed99690e54091f8a471fc88f76
radloff <bostontrader@gmail.com>
2016-07-01 07:37:21 +0800

    First draft of CRUD routes for a single collection (accounts) using mongodb.

--- accounts.js
+++ accounts.js
@@ -1,93 +1,50 @@
-module.exports = function(app, connection) {
+var ObjectId = require('mongodb').ObjectId;
+
+module.exports = function(app, db) {
 
     app.get("/accounts", function (req, res) {
-
-        var rows = connection.query("select id, title from accounts", function (err, rows, fields) {
-            if (err)
-                res.json({"error":JSON.stringify(err)});
-            else {
-                var j = {"accounts": []};
-                for (let row of rows) {
-                    var newRow = {
-                        "id": row.id,
-                        "title": row.title
-                    };
-                    j.accounts.push(newRow);
-                }
-                res.json(j);
-            }
-        });
-    });
+        db.collection('accounts').find({}).toArray().then(function resolve(result){
+            res.json(result)
+        }).catch(function reject(error){
+            console.log(error)
+            res.json({"error": error})
+        })
+    })
 
     app.get("/accounts/:id", function (req, res) {
-        var id = parseInt(req.params.id);
-        var query = "select id, title from accounts where id=? limit 1";
+        db.collection('accounts').find({'_id':ObjectId(req.params.id)}).toArray().then(function resolve(result){
+            res.json(result)
+        }).catch(function reject(error){
+            console.log(error)
+            res.json({"error": error})
+        })
+    })
 
-        var rows = connection.query(query, [connection.escape(id)], function (err, rows, fields) {
-            if (err) {
-                res.json({"error":JSON.stringify(err)});
-            } else {
-                var newRow = {
-                    "id": rows[0].id,
-                    "title": rows[0].title
-                };
-                res.json(newRow);
-            }
-        });
-    });
-
-    // By default, MySQL does not allow null values in fields so all of
-    // the fields of this record are required.
     app.post("/accounts", function(req, res) {
+        db.collection('accounts').insertOne({title: req.body.title})
+            .then(function resolve(result){
+                res.json({"result": "ok"})
+            }).catch(function reject(error){
+                console.log(error)
+                res.json({"error": error})
+            })
+    })
 
-        var title = req.body.title;
-        var query = "INSERT INTO accounts (title) VALUES (?)";
-
-        // This does the escaping for us
-        var rows = connection.query(query, [title], function (err, rows, fields) {
-            if (err)
-                res.json({"error": JSON.stringify(err)});
-            else
-                res.json({"result": "ok"});
-        });
-    });
-
-    // By default, MySQL does not allow null values in fields so all of
-    // the fields of this record are required.
     app.put("/accounts/:id", function(req, res) {
-        var id = parseInt(req.params.id);
-        escape=[];
+        db.collection('accounts').findOneAndUpdate({'_id':ObjectId(req.params.id)},{title:req.body.title}).then(function resolve(result){
+            res.json(result)
+        }).catch(function reject(error){
+            console.log(error)
+            res.json({"error": error})
+        })
+    })
 
-        var sett = "";
-        //var symbol = req.body.symbol;
-        var comma = "";
-        //if(symbol) {
-            //symbol=connection.escape(symbol);
-            //sett = "symbol=" + symbol + sett;
-            //escape.unshift(symbol);
-            //comma = ",";
-        //}
-
-        var title = req.body.title;
-        if(title) {
-            title=connection.escape(title);
-            sett = "title=" + title + comma + sett;
-            escape.unshift(title);
-        }
-
-        // id is already essentially escaped by parseInt ???
-        var query = "UPDATE accounts SET " + sett + " where id="+id;
-
-        // We have to do the escaping manually because otherwise we get
-        // parse errors.
-        //var rows = connection.query(query, [escape], function (err, rows,
-        // fields) {
-        var rows = connection.query(query, function (err, rows, fields) {
-            if (err)
-                res.json({"error": JSON.stringify(err)});
-            else
-                res.json({"result": "ok"});
-        });
-    });
-
+    app.delete("/accounts/:id", function(req, res) {
+        db.collection('accounts').findOneAndDelete({'_id':ObjectId(req.params.id)}).then(function resolve(result){
+            res.json(result)
+        }).catch(function reject(error){
+            console.log(error)
+            res.json({"error": error})
+        })
+    })
 }
--- app.js
+++ app.js
@@ -1,77 +1,40 @@
-var express = require("express");
-var http = require("http");
-var logger = require("morgan");
-var mysql = require('mysql');
-var bodyParser = require('body-parser')
+'use strict'
 
-var app = express();
-module.exports = app; // export this for testing
+let assert = require('assert')
+let bodyParser = require('body-parser')
+let express = require("express")
+let http = require("http")
+let logger = require("morgan")
 
-var dbCredentials = require("./database").dbCredentials;
-var dbConnection  = mysql.createConnection(dbCredentials);
-dbConnection.connect();
+let app = express()
 
-//app.use(logger("short")); // morgan logger
+let MongoClient = require('mongodb').MongoClient
+let mongoURL = 'mongodb://localhost:27017/bookwerx-test'
 
-app.use(bodyParser.urlencoded());
+//app.use(logger("short"))
 
-app.set("port", process.env.PORT || 3001);
+app.use(bodyParser.urlencoded())
 
-app.get("/", function(req, res) {
-    res.type("text");
-    res.send(req.headers["user-agent"]);
-});
+app.set("port", process.env.PORT || 3006)
 
-// This is good for testing, but why would we ever want to use this in real
-// life? If we want to empty all the tables, better to use some other admin
-// tool.
-//
-// TRUNCATE will fail if we have foreign key constraints.  We _might_ be able
-// to turn off the constraints, TRUNCATE, and then turn the constraints back on,
-// but I haven't explored that route.
-//
-// DELETE works just fine, but we have to be careful to delete the rows from
-// the tables in the correct order, in order to keep the constraints happy.
-app.post("/brainwipe", function(req, res) {
+// Connect to the mongo server...
+MongoClient.connect(mongoURL).then(function resolve(mongoDb){
+    // And then setup the routes, because the routes need access to the mongodb
+    require("./accounts")(app, mongoDb)
 
-    // Be careful to list these in the order of deletion, in order to keep
-    // the constraints happy
-    dbConnection.query("delete from distributions", function (err, rows, fields) {
-        if (err) res.json({"error":JSON.stringify(err)})});
+    app.post("/brainwipe", function (req, res) {
+        mongoDb.dropDatabase().then(function(result) {
+            res.json({"result":"ok"});
+        }).catch(function(error){
+            res.json({"error":JSON.stringify(err)});
+        })
+    })
 
-    dbConnection.query("delete from transactions", function (err, rows, fields) {
-        if (err) res.json({"error":JSON.stringify(err)})});
+    // And we don't want to start listening before the routes are setup
+    app.listen(app.get("port"), function() {
+        console.log("App started on port " + app.get("port"))
+    })
 
-    dbConnection.query("delete from currencies", function (err, rows, fields) {
-        if (err) res.json({"error":JSON.stringify(err)})});
-
-    dbConnection.query("delete from accounts_categories", function (err, rows, fields) {
-        if (err) res.json({"error":JSON.stringify(err)})});
-
-    dbConnection.query("delete from accounts", function (err, rows, fields) {
-        if (err) res.json({"error":JSON.stringify(err)})});
-
-    dbConnection.query("delete from categories", function (err, rows, fields) {
-        if (err) res.json({"error":JSON.stringify(err)})});
-
-    res.json({"result":"ok"});
-});
-
-var accounts_routes = require("./accounts");
-accounts_routes(app, dbConnection);
-
-var categories_routes = require("./categories");
-categories_routes(app, dbConnection);
-
-var currencies_routes = require("./currencies");
-currencies_routes(app, dbConnection);
-
-//var distributions_routes = require("./distributions");
-//distributions_routes(app, dbConnection);
-
-var transactions_routes = require("./transactions");
-transactions_routes(app, dbConnection);
-
-app.listen(app.get("port"), function() {
-    console.log("App started on port " + app.get("port"));
-});
+}).catch(function reject(error){
+    console.log(error)
+})
