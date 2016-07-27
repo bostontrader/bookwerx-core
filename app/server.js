4de21ae4b07f7600428302b4807589b098dedf95
radloff <bostontrader@gmail.com>
2016-07-27 11:16:56 +0800

    Brainwipe and start over with restify.

--- app/server.js
+++ app/server.js
@@ -1,34 +1,14 @@
-#!/usr/bin/env node
+let restify = require('restify')
 
-// Express, Jade, routes... stuff we need.  Factor this out because the main
-// and the test app both need this.
-var app = require('./bootstrap')
+function respond (req, res, next) {
+  res.send('hello ' + req.params.name)
+  next()
+}
 
-// Does test-app not need config or db?
-var config = require('./config')
-var db     = require('./db')
+var server = restify.createServer()
+server.get('/hello/:name', respond)
+server.head('/hello/:name', respond)
 
-// Setup logging.
-var bole = require('bole')
-bole.output({level: 'debug', stream: process.stdout})
-var log = bole('server')
-
-log.info('server process starting')
-
-// Connect to Mongo on start.  If successful, then start listening
-db.connect(`mongodb://${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.dbname}`, err => {
-//db.connect('mongodb://localhost:27017/bookwerx-core', function(err) {
-  if (err) {
-    console.log('Unable to connect to Mongo.')
-    process.exit(1)
-  } else {
-    app.listen(config.express.port, config.express.ip, err => {
-      if (err) {
-        log.error('Unable to listen for connections', err)
-        process.exit(10)
-      }
-      log.info('express is listening on http://' +
-          config.express.ip + ':' + config.express.port)
-    })
-  }
+server.listen(3003, () => {
+  console.log('%s listening at %s', server.name, server.url)
 })
--- technotes.md
+++ technotes.md
@@ -1,33 +1,20 @@
-##package.json
+after any change of branch, be sure to npm prune and npm install
 
-  npm prune will remove items no longer referenced by package.json
+npm prune will remove items no longer referenced by package.json
+
+
+##package.json
 
 ###dependencies
 
 ###Production dependencies
 
-  **body-parser** Need this to get the body of HTTP responses and parse
-  into JSON.
+  **restify** This is our server framework.
 
-  **bole** JSON logger.
-  
-  **express** This is our application framework.
-
-  **jade**
-  
   **mongodb** Need this to connect to our mongodb.
-
-  **morgan** Another logger.  Do we really need two of them?
 
 ###Development/testing dependencies
 
-    **bistre** Formatter for the bole logger.
-    
-    **standard** JS Linter.
+  **standard** A linter
 
-    **supertest**
-
-    **tap-dot** Test output formatter
-
-    **tape** Testing system
-  
+  **tape** A tester
