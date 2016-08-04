f054151e978ce6791dfdaf5369d2d30509c2674e
radloff <bostontrader@gmail.com>
2016-08-05 06:54:00 +0800

    Log the name of the configuration used, upon startup.

--- app/server.js
+++ app/server.js
@@ -32,6 +32,7 @@ MongoClient.connect(mongoConnectionURL)
       transactionsRouter.defineRoutes(server, mongoDb)
 
       server.listen(port, () => {
+        console.log('Using configuration: %s',config.get('configName'))
         console.log('%s listening at %s', server.name, server.url)
         resolve(true)
       })
