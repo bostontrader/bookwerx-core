fede0b51ba92e2736072cdba2c201b043f01a81b
radloff <bostontrader@gmail.com>
2016-08-26 15:12:27 +0800

    Minor fixes and cleanup.

--- app/distributions/routing.js
+++ app/distributions/routing.js
@@ -1,4 +1,5 @@
 let genericRoutes = require('../generic_routes')
+let ObjectId = require('mongodb').ObjectId
 let collectionSingular = 'distribution'
 let collectionPlural = 'distributions'
 
@@ -25,7 +26,9 @@ exports.defineRoutes = function (server, mongoDb) {
     if (!req.body.transaction_id) {
       res.json({error: 'A distribution must have a transaction_id'})
     }
-
+    req.body.account_id = ObjectId(req.body.account_id)
+    req.body.currency_id = ObjectId(req.body.currency_id)
+    req.body.transaction_id = ObjectId(req.body.transaction_id)
     mongoDb.collection(collectionPlural).insertOne(req.body).then(result => {
       retVal._id = result.insertedId.toString()
       res.json(retVal)
@@ -34,6 +37,24 @@ exports.defineRoutes = function (server, mongoDb) {
     })
   })
 
-  genericRoutes.put(server, mongoDb, collectionSingular, collectionPlural)
+  // Override genericRoutes because of special integrity constraint checking
+  // genericRoutes.put(server, mongoDb, collectionSingular, collectionPlural)
+  server.put('/' + collectionPlural + '/:id', (req, res, next) => {
+    req.body.account_id = ObjectId(req.body.account_id)
+    req.body.currency_id = ObjectId(req.body.currency_id)
+    req.body.transaction_id = ObjectId(req.body.transaction_id)
+    mongoDb.collection(collectionPlural).findOneAndUpdate(
+      {'_id': ObjectId(req.params.id)},
+      req.body,
+      {returnOriginal: false}
+    )
+    .then(function resolve (result) {
+      if (result.value === null) result.value = {error: collectionSingular + ' ' + req.params.id + ' does not exist'}
+      res.json(result.value)
+    }).catch(error => {
+      res.json({'error': error})
+    })
+  })
+
   genericRoutes.delete(server, mongoDb, collectionSingular, collectionPlural)
 }
--- app/transactions/routing.js
+++ app/transactions/routing.js
@@ -1,4 +1,5 @@
 let genericRoutes = require('../generic_routes')
+let ObjectId = require('mongodb').ObjectId
 let collectionSingular = 'transaction'
 let collectionPlural = 'transactions'
 
@@ -8,4 +9,72 @@ exports.defineRoutes = function (server, mongoDb) {
   genericRoutes.post(server, mongoDb, collectionPlural)
   genericRoutes.put(server, mongoDb, collectionSingular, collectionPlural)
   genericRoutes.delete(server, mongoDb, collectionSingular, collectionPlural)
+
+  /*
+   The transaction dashboard contains a list of all distributions for a particular transaction,
+   listed in no particular order, as well as other various joined fields such as a currency symbol.
+   In addition, we also want info from the transaction document itself.
+
+   More particularly, we want:
+
+   {
+     "transaction":{
+        "_id": ...,
+        "datetime": ...,
+        "note": ...
+      },
+      "distributions": {[
+        "_id": ...,
+        "account_id":"123",
+        "currency_id":"456",
+        "amount":500,
+        "transaction_id":"nnn",
+        "account":[
+          {"_id":"123","title":"Cash in Mattress"}
+        ],
+        "currency":[
+          {"_id":"456","symbol":"RMB","title":"Ren Min Bi"}
+        ]
+      ]}
+   }
+
+   We produce this with two queries to the db.  One to get the transaction info, and one to get the distribution (and
+   joined) info.
+
+   Even though account and currency are arrays (cuz that's how mongo rolls), in this app, there should only
+   be exactly one of each.
+
+   */
+  server.get('/transactions/dashboard/:transaction_id', (req, res, next) => {
+    let distributions
+    return mongoDb.collection('distributions')
+      .aggregate([
+        {$match: {transaction_id: ObjectId(req.params.transaction_id)}},
+        {
+          $lookup: {
+            from: 'accounts',
+            localField: 'account_id',
+            foreignField: '_id',
+            as: 'account'
+          }
+        },
+        {
+          $lookup: {
+            from: 'currencies',
+            localField: 'currency_id',
+            foreignField: '_id',
+            as: 'currency'
+          }
+        }
+      ]).toArray()
+    .then(result => {
+      distributions = result
+      return mongoDb.collection('transactions').findOne({'_id': ObjectId(req.params.transaction_id)})
+    })
+    .then(result => {
+      res.json({'distributions': distributions, 'transaction': result})
+    }).catch(error => {
+      res.json({'error': error})
+    })
+  })
 }
