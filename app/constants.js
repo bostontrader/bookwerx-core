2310047ac36fa11694dab061a0ca6bb493c589cd
Thomas Radloff <bostontrader@gmail.com>
2018-07-11 07:37:01 +0800

    Tweak error messages.

--- app/constants.js
+++ app/constants.js
@@ -10,8 +10,8 @@ module.exports = {
   ATTEMPTED_IMPLICIT_CREATE: 'the request tries to update a non-existing document',
 
   // Runtime configuration
-  NO_LISTENING_PORT_DEFINED: 'No listening port defined',
-  NO_CONNECTION_TO_MONGODB_DEFINED: 'No connection to mongodb defined',
-  NO_BWCORE_HOSTNAME_DEFINED: 'No bookwerx-core hostname is defined',
-  NOT_CONFIGURED_AS_TEST: 'This execution is not configured to be a test  Please set the BW_TEST env variable'
+  NO_LISTENING_PORT_DEFINED: 'No listening port defined (BW_PORT)',
+  NO_CONNECTION_TO_MONGODB_DEFINED: 'No connection to mongodb defined (BW_MONGO)',
+  NO_BWCORE_HOSTNAME_DEFINED: 'No bookwerx-core hostname is defined (BWCORE_HOSTNAME)',
+  NOT_CONFIGURED_AS_TEST: 'This execution is not configured to be a test (BW_TEST)'
 }
